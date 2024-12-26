1b768228011658562d78a10617a6adaf7e482924
shawaiziqbal <140547247+shawaiziqbal@users.noreply.github.com>
2024-12-26 17:32:17 +0500

    login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="card">
+    <div class="card-body">Panel Content</div>
+    <div class="card-footer">Panel</div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- Navbar Brand -->
+        <a class="navbar-brand" href="#">E-comm</a>
+        
+        <!-- Toggler Button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" 
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <!-- Navbar Links -->
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+            </ul>
+            
+            <!-- Search Form -->
+            <form class="d-flex me-3">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary" type="submit">Search</button>
+            </form>
+            
+            <!-- Right-Aligned Links -->
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Add to Cart</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
